10845a28a29ed22c4da9e8cf4d357b05fe3e185c
subhrajyoti-basu <subhrajyoti.basu@outlook.com>
2024-09-04 15:19:57 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {unoptimized: true}
+    images: {unoptimized: true},
+    basePath: '/wizsuby.github.io',
+  assetPrefix: '/wizsuby.github.io/'
 }
 
 module.exports = nextConfig
